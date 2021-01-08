c1796b1b99fae9678faace41dcf6d561d9522142
Raj <raj.rajeev2244@gmail.com>
2021-01-08 21:36:22 +0530

    Initial Commit

--- Demo.java
+++ Demo.java
@@ -2,5 +2,6 @@ public class Demo
 {
     public static void main(String[] args) {
         System.out.println("Welcome TO Java")
+        System.out.println("Current Date And Time :"+new java.util.Date());
     }
 }
